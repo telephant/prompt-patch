4c3995fb23d4965013f78c9df803c00b9d9ffbbf
telephant11 <telephant11@gmail.com>
2025-07-16 18:06:59 +0400

    fix: Update config of nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // enable static export
+  images: {
+    unoptimized: true, // close image optimization
+  },
 };
 
 export default nextConfig;
